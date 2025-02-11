636aee4bb8f13e6032a0afed71b1cbf32119c01b
Richard Gagg <r.gagg@outlook.com>
2025-02-11 15:07:34 +1100

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Richard Gagg on 11/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
